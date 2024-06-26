cddd1e20e90e7af77aaefcfcdd5547567eef148a
Manvitha <165771992+Manvitha212005@users.noreply.github.com>
2024-06-26 18:15:05 +0530

    Add files via upload

---
+++ day5bootcamp.py
@@ -0,0 +1,83 @@
+"""
+first order dunction
+-function can be stored to variables
+-function cam be passed ad a parameter
+-functyoon can be returned
+"""
+"""
+#HIGH ORDER FUNCTION
+-func which accepts func as a parameter"""
+"""
+def add():
+    print("add")
+x=add
+print(x())
+
+
+
+def printtest(a):
+    print(a)
+def test(w):
+    w("i am printing")
+#fixed set input gives fixed set of output
+
+test(printtest)
+"""
+"""
+-pure function are godal 
+"""
+"""
+def add(x,y):
+     return x+y
+x=9
+y=9
+x=add(x,y)
+"""
+"""
+x=list(map(int,input().split(", ")))
+y=[]
+m=Math.sqrt(x)
+m.append(y)
+print(y)"""
+"""
+a=[1,2,3,4]         
+def sq(i):
+    return i*i
+print(list(map(sq,a)))
+"""
+"""
+a=["hi","manvi"]
+
+def cap(i):
+   return i[0].upper()+i[1:]
+print(list(map(cap,a)))
+"""
+"""
+
+#split func
+filter and check condidtions and make new list"""
+"""
+a=[4,5,67]
+def f(i):
+    print(list(filter(f,a)))"""
+""" mP WORKS for all the values
+    mapping for all of them
+    filter is based on condition"""
+"""a=[1,2,3,4]
+def f(i):
+    return i%5==0
+print(list(filter(f,a)))"""
+"""a=["in","am","manvi"]
+def me(i):
+    return len(i)==2
+print(list(filter(me,a)))"""
+"""from functools import reduce
+def joining(x,y):
+    return x+" "+y
+reduce(joining,a,'0')
+zip make tuples
+"""
+
+    
+    
+    
